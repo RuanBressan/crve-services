4e8599e51e9c24fd8da06bc8824c3aa49cfc42d8
RUAN DE SOUZA BRESSAN <ruan_bressan@estudante.sesisenai.org.br>
2025-04-01 14:27:19 -0300

    Primeiro Commit

---
+++ crvservices/src/main/java/com/crveservices/crvservices/CrvservicesApplication.java
@@ -0,0 +1,13 @@
+package com.crveservices.crvservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrvservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrvservicesApplication.class, args);
+	}
+
+}
---
+++ crvservices/src/test/java/com/crveservices/crvservices/CrvservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crveservices.crvservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrvservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
